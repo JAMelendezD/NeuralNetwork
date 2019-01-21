1143aabd9058300252b615317e16b39e829f4461
JAMelendezD <32931128+JAMelendezD@users.noreply.github.com>
2019-01-21 16:36:42 -0500

    Update network.py

--- network.py
+++ network.py
@@ -10,7 +10,6 @@ def load_data():
     f.close()
     return (training_data,validation_data,test_data)
 
-
 def load_data_wrapper():
     tr_d, va_d, te_d = load_data()
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
@@ -22,16 +21,13 @@ def load_data_wrapper():
     test_data = zip(test_inputs, te_d[1])
     return (training_data, validation_data, test_data)
 
-
 def vectorized_result(j):
     e = np.zeros((10, 1))
     e[j] = 1.0
     return e
 
-
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
-
 
 def sigmoid_prime(z):
     return sigmoid(z)*(1-sigmoid(z))
@@ -48,12 +44,10 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
-
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
-
 
     def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
 
@@ -74,7 +68,6 @@ class Network(object):
                 print("Epoch {} : {} / {}".format(j,self.evaluate(test_data),n_test));
             else:
                 print("Epoch {} complete".format(j))
-
 
     def update_mini_batch(self, mini_batch, eta):
 
@@ -115,11 +108,9 @@ class Network(object):
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
 
-
     def evaluate(self, test_data):
         test_results = [(np.argmax(self.feedforward(x)), y) for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
-
 
 
 training_data , validation_data , test_data = load_data_wrapper()
